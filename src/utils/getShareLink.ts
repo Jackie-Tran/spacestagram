4520ef71c06c2d1273ddb2b7bf04ced6859ed267
Jackie <jtran2009a@gmail.com>
2022-01-17 11:33:10 -0500

    fixed share link

--- src/utils/getShareLink.ts
+++ src/utils/getShareLink.ts
@@ -1,6 +1,11 @@
 const getShareLink = (date: string) => {
   const link = `${window.location.protocol}//${window.location.host}/${date}`;
-  navigator.clipboard.writeText(link);
+  const temp = document.createElement('textarea');
+  temp.value = link;
+  document.body.appendChild(temp);
+  temp.select();
+  document.execCommand('copy');
+  document.body.removeChild(temp);
 };
 
 export default getShareLink;
