b43afaa2b206e89ff25567b8777a1446a12fd509
Jackie <jtran2009a@gmail.com>
2022-01-17 02:00:19 -0500

    fixed toast timer

--- src/hooks/useToastMessage.tsx
+++ src/hooks/useToastMessage.tsx
@@ -1,13 +1,15 @@
-import { useContext } from 'react';
+import { useContext, useRef } from 'react';
 import ToastContext from '../context/ToastContext';
 
 const useToastMessage = () => {
   const { setMessage, setVisible } = useContext(ToastContext);
+  const exitTimer = useRef<number>();
 
   const showMessage = (toastMessage: string) => {
+    if (exitTimer.current) window.clearTimeout(exitTimer.current);
     setMessage(toastMessage);
     setVisible(true);
-    setTimeout(() => {
+    exitTimer.current = window.setTimeout(() => {
       setVisible(false);
     }, 4000);
   };
